a067be45146ff4f5e4fed02e96f2f4364b9310ee
S. M. Catala <stephane@zenyway.com>
2016-06-10 14:26:45 +0200

    extend module test specs and implement module skeleton accordingly fix and upgrade travis-ci environment
    Squashed commit of the following:
    
    commit fb3bdd2389eca6a8344e3cae7d2522c86092bd76
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Fri Jun 10 13:38:16 2016 +0200
    
        remove useless Cryptobox constructor assignment
    
    commit dbcd971a1f607deb3d6bd514e0482a340c3f6e5b
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Fri Jun 10 13:06:37 2016 +0200
    
        fix travis config
        upgrade to ubuntu trusty and replace chromium with chrome
    
        Squashed commit of the following:
    
        commit 429227fac997db86f2837e6764397e615ec548f3
        Author: S. M. Catala <stephane@zenyway.com>
        Date:   Fri Jun 10 12:56:27 2016 +0200
    
            remove debug logs
    
        commit 8ca1e0debd2292ab0049e8fd4d9f05c64ce377a1
        Author: S. M. Catala <stephane@zenyway.com>
        Date:   Fri Jun 10 12:50:28 2016 +0200
    
            upgrade travis config to ubuntu trusty for chrome install
    
        commit 12922634da82538edbd073a1fd13a46f23bed917
        Author: S. M. Catala <stephane@zenyway.com>
        Date:   Fri Jun 10 12:36:43 2016 +0200
    
            specify amd64 arch for google sources.list in travis config
    
        commit 63b39434d7ff9a8a8b1e135d4ce68052aeb7ca83
        Author: S. M. Catala <stephane@zenyway.com>
        Date:   Fri Jun 10 12:20:05 2016 +0200
    
            replace chromium with chrome in travis config
    
        commit 9877588b19bdc9b0b27c6a481e42e916be80840d
        Author: S. M. Catala <stephane@zenyway.com>
        Date:   Fri Jun 10 11:50:35 2016 +0200
    
            add travis config to update chromium-browser before install
    
        commit 088a3360528e101cf573335c9cc9920bc2386858
        Author: S. M. Catala <stephane@zenyway.com>
        Date:   Fri Jun 10 11:36:09 2016 +0200
    
            add debug log in test for Cbox immutability
    
    commit 5dfdbab3a8da69af941b14a8a2d7d5d976967850
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Fri Jun 10 11:22:55 2016 +0200
    
        add object-assign to typings.json
        tentatively solve travis-ci compilation failure
    
    commit 0994931f62dbd00e8dc98c57d531c17e05b2d326
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Fri Jun 10 09:32:02 2016 +0200
    
        replace object.assign ponyfill with object-assign module
        tentatively solve travis-ci chromium compatibility
    
    commit 59fd10550b1af59259c28143010b1a04db5a2618
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Fri Jun 10 00:41:37 2016 +0200
    
        add missing object.assign polyfill
    
    commit c6da73e917998d86c74fad0372063f928f4cad3f
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Fri Jun 10 00:36:06 2016 +0200
    
        implement cryptobox factory skeleton and debug tests
        all tests pass
    
    commit 34dea1138b46a13a9dcc2a32232c9d24ad300ff5
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Wed Jun 8 15:06:27 2016 +0200
    
        change factory type assertions from "as any" to "<Function>"
    
    commit ea1ee828a0f2f01f4538cb52d3dc96493d93cf39
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Tue Jun 7 18:32:50 2016 +0200
    
        add doc comments
    
    commit b8e721aa52abb35f933d48e657fc32718fb33d48
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Tue Jun 7 16:52:36 2016 +0200
    
        remove sourceRoot option for build compilation
    
    commit 83c8a36ffdf38359f0d5bd03707cd0bcf9f35b04
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Tue Jun 7 16:51:04 2016 +0200
    
        update spec of module factory return value
    
    commit b6a5b7607946fd46f1fe8f5fa6868bb03337e492
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Tue Jun 7 14:47:15 2016 +0200
    
        update type structure exported from module
    
        * update Cryptoboxes to union of interface and namespace
        * add Config and Deps interfaces to Cryptoboxes namespace
        * update test spec accordingly
    
    commit f257d429ce17ddcaa5a67b65107e6bd5b50d3b1f
    Author: S. M. Catala <stephane@zenyway.com>
    Date:   Tue Jun 7 00:22:49 2016 +0200
    
        extend Cryptobox module tests
    
        * specify config object argument constraints
        * specify defensive copy of config object argument and return value

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,129 @@
-export default function () {
-  // TODO
+/// <reference path="../typings/index.d.ts" />
+import assign = require('object-assign')
+
+/**
+ * Copyright 2016 Stephane M. Catala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * Limitations under the License.
+ */
+
+/**
+ * @public
+ * @factory
+ * @param {url: string, id: string} config
+ * @return {Cryptoboxes}
+ * @throws Error 'invalid credentials' when
+ * - creds is not a valid credentials object
+ * - or a cryptobox instance already exists for the given creds.id
+ */
+export default function getFactory (config: Config): Cryptoboxes {
+  if (!config || (typeof config.url !== 'string')
+  || (typeof config.agent !== 'string')) {
+    throw new Error('invalid argument')
+  }
+
+  return Cryptobox.prototype.cryptoboxes = Object.freeze({
+    create: create,
+    access: access,
+    config: Object.freeze(assign({}, config)) // defensive copy
+  })
+}
+
+export interface Config {
+  url: string
+  agent: string
+}
+
+export interface Creds {
+  id: string,
+  secret: string
+}
+
+export interface Cryptoboxes {
+  create (creds: Creds): Cryptobox
+  access (creds: Creds): Cryptobox
+  config: Config
+}
+
+export interface Cryptobox {
+  cryptoboxes: Cryptoboxes
+}
+
+let _pool = Object.create(null) // TODO should be local pouchdb instances
+
+/**
+ * @public
+ * @factory
+ * @param {id: string, secret: string} creds
+ * @return {Cryptobox} new immutable instance for given creds
+ * @throws Error 'invalid credentials' when
+ * - creds is not a valid credentials object
+ * - or a cryptobox instance already exists for the given creds.id
+ */
+function create (creds: Creds): Cryptobox {
+  if (!isValidType(creds) || (creds.id in _pool)) {
+    throw new Error('invalid credentials')
+  }
+
+  let _creds = Object.freeze({ // defensive copy
+    id: creds.id,
+    hash: creds.secret // TODO SHA256(creds.secret)
+  })
+
+  let cryptobox = Object.freeze(Object.create(Cryptobox.prototype))
+
+  _pool[_creds.id] = function access (creds: Creds): Cryptobox {
+    if (!isValidType(creds) || (creds.id !== _creds.id)
+    || (creds.secret !== _creds.hash)) { // TODO SHA256(creds.secret) !== _creds.hash
+      throw new TypeError('invalid credentials')
+    }
+    return cryptobox
+  }
+
+  return cryptobox
+}
+
+/**
+ * @public
+ * @factory
+ * @param {id: string, secret: string} creds
+ * @return {Cryptobox} for given creds
+ * @throws Error 'invalid credentials' when
+ * - creds is not a valid credentials object
+ * - or there is no cryptobox instance for the given creds.id
+ * - or creds does not match that of the corresponding cryptobox instance
+ */
+function access (creds: Creds): Cryptobox {
+  if (!isValidType(creds) || !(creds.id in _pool)) {
+    throw new Error('invalid credentials')
+  }
+
+  return _pool[creds.id](creds)
+}
+
+/**
+ * @private
+ * @param {id: string, secret: string} creds
+ * @return {boolean} true if creds is a valid credentials object
+ */
+function isValidType(creds: Creds) {
+  return creds && (typeof creds.id === 'string')
+    && (typeof creds.secret === 'string')
+}
+
+/**
+ * @private
+ * @constructor
+ * @throws Error 'Unauthorized' when called
+ */
+function Cryptobox () {
+  throw new Error('Unauthorized')
 }
